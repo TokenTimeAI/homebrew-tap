3cc9ba6a0c865e1356e478c4349b1d5aec3153da
Pandelis Zembashis <p@ndel.is>
2026-06-12 19:04:05 -0700

    Update ttime to v0.1.6

--- Formula/ttime.rb
+++ Formula/ttime.rb
@@ -1,28 +1,28 @@
 class Ttime < Formula
   desc "Local heartbeat daemon client for ttime.ai"
   homepage "https://ttime.ai"
-  version "0.1.5"
+  version "0.1.6"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/TokenTimeAI/client/releases/download/v#{version}/ttime_Darwin_arm64.tar.gz"
-      sha256 "b66dcf3ce60cc556add6dd18de620c3126abf67326431eb5ef02d73a81ad61f8"
+      sha256 "3773d6fc740c12fe5d305e3e5b37f970a2041507ae13261c68c275f7f6cda713"
     end
     on_intel do
       url "https://github.com/TokenTimeAI/client/releases/download/v#{version}/ttime_Darwin_x86_64.tar.gz"
-      sha256 "1db1e537652e142bdf72f8170a6c9b02d66926dcfcb92a04fa6dcba8fabfa1a7"
+      sha256 "51bbd15ea90d9d0419bf2d780eea1d2c22194e00b62ea9575d8824844f2a239b"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/TokenTimeAI/client/releases/download/v#{version}/ttime_Linux_arm64.tar.gz"
-      sha256 "c56283992923fa79876c0fd1fefc520d3448adb6c52cc866348d842dcef9e3f6"
+      sha256 "b6f6baf7ff0f03a3ffc43697f80dfd8e46db5bcfb51a091ee3b2dc7088d7a4dd"
     end
     on_intel do
       url "https://github.com/TokenTimeAI/client/releases/download/v#{version}/ttime_Linux_x86_64.tar.gz"
-      sha256 "331099a5781e0711a74d8dfbfd4448bfd5bd41846e470f23afb374755faec1c0"
+      sha256 "05d41f2551cd3ff4ffee8b997547c10d6260d60b3ba98f22f9366bfd6801e411"
     end
   end
 
